f97a4c56931837827403a43c251c7352821f0db4
root <root@localhost.localdomain>
2025-11-21 15:33:35 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes!");
-});
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`App running on port ${port}`));
 
-app.listen(3000, () => console.log("App running on port 3000"));
